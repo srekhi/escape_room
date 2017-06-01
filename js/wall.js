157bc4b4bfe7fee8f47a272d30181903adc3a2f8
Sunny Rekhi <rohit.rekhi@gmail.com>
2017-06-01 10:09:14 -0700

    Add canvas height sizing

--- js/wall.js
+++ js/wall.js
@@ -15,7 +15,7 @@ class Wall {
     context.fillStyle = "black";
     context.fillRect(this.x, this.y, this.width, this.height);
     context.closePath();
-    context.stroke();
+    context.stroke(); 
     // context.strokeRect(this.x, this.y, this.width, this.height);
   }
 }
